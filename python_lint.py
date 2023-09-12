cde0cbbe8af22618bd753c5668d353009c49bac0
aegilops <41705651+aegilops@users.noreply.github.com>
2023-09-12 18:26:49 +0100

    Can now run all 3 linters at the same time

--- python_lint.py
+++ python_lint.py
@@ -30,32 +30,30 @@ def flake8_linter(target: Path, output_filename: str) -> None:
     """
     LOG.debug("Running flake8")
 
-    from flake8 import __version__ as flake8_version
-    from flake8.api import legacy as flake8_api
+    process = run(["flake8", target, "--format", "sarif"], capture_output=True)
 
-    LOG.debug("flake8 version: %s", flake8_version)
+    if process.stderr:
+        LOG.error("STDERR: %s", process.stderr.decode("utf-8"))
+        return None
 
-    # run flake8, outputting SARIF
-    runner = flake8_api.get_style_guide(format="sarif", output_file=output_filename)
-    runner.check_files([target.absolute().as_posix()])
+    if not process.stdout:
+        LOG.error("No output from flake8")
+        return None
 
-    return None
+    # process STDOUT
+    sarif = json.loads(process.stdout.decode("utf-8"))
+
+    return sarif["runs"][0]
 
 
 def ruff_format_sarif(results: list[dict[str, Union[str,int]]]) -> dict:
-    sarif = {
-        "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
-        "version": "2.1.0",
-        "runs": [
-            {
-                "tool": {
-                    "driver": {
-                        "name": "Ruff",
-                    }
-                },
-                "results": [
-                ],
+    sarif_run = {
+        "tool": {
+            "driver": {
+                "name": "Ruff",
             }
+        },
+        "results": [
         ],
     }
 
@@ -85,9 +83,9 @@ def ruff_format_sarif(results: list[dict[str, Union[str,int]]]) -> dict:
             # Code Scanning doesn't do anything with it, so it isn't a high priority
         }
 
-        sarif["runs"][0]["results"].append(sarif_result)
+        sarif_run["results"].append(sarif_result)
 
-    return sarif
+    return sarif_run
 
 
 def ruff_linter(target: Path, *args) -> dict:
@@ -102,42 +100,36 @@ def ruff_linter(target: Path, *args) -> dict:
 
     if process.stderr:
         LOG.error("STDERR: %s", process.stderr.decode("utf-8"))
-        return
+        return None
 
     if not process.stdout:
         LOG.error("No output from ruff")
-        return
+        return None
 
     # process STDOUT
     results = json.loads(process.stdout.decode("utf-8"))
 
     # format the ruff JSON into SARIF
-    sarif = ruff_format_sarif(results)
+    sarif_run = ruff_format_sarif(results)
 
-    return sarif
+    return sarif_run
 
 
 def pylint_format_sarif(results: list[dict[str, Union[str,int]]], target: Path) -> dict:
-    sarif = {
-        "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
-        "version": "2.1.0",
-        "runs": [
-            {
-                "tool": {
-                    "driver": {
-                        "name": "Pylint",
-                        "rules": [],
-                    }
-                },
-                "results": [
-                ],
+    sarif_run = {
+        "tool": {
+            "driver": {
+                "name": "Pylint",
+                "rules": [],
             }
+        },
+        "results": [
         ],
     }
 
     for result in results:
         # append rule ID and label to the SARIF
-        rules = sarif["runs"][0]["tool"]["driver"]["rules"]
+        rules = sarif_run["tool"]["driver"]["rules"]
         if result["message-id"] not in [rule["id"] for rule in rules]:
             sarif_rule = {
                 "id": result["message-id"],
@@ -174,9 +166,9 @@ def pylint_format_sarif(results: list[dict[str, Union[str,int]]], target: Path) 
             # Code Scanning doesn't do anything with it, so it isn't a high priority
         }
 
-        sarif["runs"][0]["results"].append(sarif_result)
+        sarif_run["results"].append(sarif_result)
 
-    return sarif
+    return sarif_run
 
 
 def pylint_linter(target: Path, *args) -> dict:
@@ -187,11 +179,11 @@ def pylint_linter(target: Path, *args) -> dict:
 
     if process.stderr:
         LOG.error("STDERR: %s", process.stderr.decode("utf-8"))
-        return
+        return None
 
     if not process.stdout:
         LOG.error("No output from pylint")
-        return
+        return None
 
     # process STDOUT
     results = json.loads(process.stdout.decode("utf-8"))
@@ -199,17 +191,46 @@ def pylint_linter(target: Path, *args) -> dict:
     LOG.debug(results)
 
     # format the pylint JSON into SARIF
-    sarif = pylint_format_sarif(results, target)
+    sarif_run = pylint_format_sarif(results, target)
 
-    return sarif
+    return sarif_run
 
 
-LINTERS = {"pylint": pylint_linter, "ruff": ruff_linter, "flake8": flake8_linter}
+def mypy_format_sarif(results: str) -> dict:
+    """Convert JUnit XML output into SARIF."""
+    return {}
+
+
+def mypy_linter(target: Path, output_filename: str) -> dict:
+    """Run the mypy linter."""
+
+    LOG.debug("Running mypy")
+
+    mypy_junit_xml = Path(output_filename).with_suffix(".xml")
+
+    process = run(["mypy", "--ignore-missing-imports", "--junit-xml", mypy_junit_xml.as_posix(), target.absolute().as_posix()], capture_output=True)
+
+    if process.stderr:
+        LOG.error("STDERR: %s", process.stderr.decode("utf-8"))
+        return None
+
+    if not process.stdout:
+        LOG.error("No output from mypy")
+        return None
+
+    LOG.debug(open(mypy_junit_xml).read())
+
+    sarif_run = mypy_format_sarif(mypy_junit_xml)
+
+    return sarif_run
+
+
+LINTERS = {"pylint": pylint_linter, "ruff": ruff_linter, "flake8": flake8_linter, "mypy": mypy_linter}
 
 
 def add_args(parser: ArgumentParser) -> None:
     """Add arguments to the parser."""
-    parser.add_argument("linters", choices=LINTERS.keys(), nargs="+", help="The linter(s) to use")
+    parser.add_argument("linter", choices=LINTERS.keys(), nargs="+", help="The linter(s) to use")
     parser.add_argument("--target", "-t", default=".", required=False, help="Target path for the linter")
     parser.add_argument("--output", "-o", default="python_linter.sarif", required=False, help="Output filename")
     parser.add_argument("--debug", "-d", action="store_true", required=False, help="Enable debug logging")
@@ -223,16 +244,28 @@ def main() -> None:
 
     logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
 
-    if args.linter not in LINTERS:
-        LOG.error(f"Invalid linter: {args.linter}")
+    if any([linter not in LINTERS for linter in args.linter]):
+        LOG.error(f"Invalid linter choice: {args.linter}")
         exit(1)
 
-    sarif = LINTERS[args.linter](Path(args.target).absolute(), args.output)
+    sarif = {
+        "$schema": "https://raw.githubusercontent.com/oasis-tcs/sarif-spec/master/Schemata/sarif-schema-2.1.0.json",
+        "version": "2.1.0",
+        "runs": [
+        ]
+    }
 
-    if sarif is not None:
-        # output the results to a file, for later upload
-        with open(args.output, "w") as sf:
-            json.dump(sarif, sf, indent=2)
+    for linter in args.linter:
+        LOG.info("Running %s", linter)
+
+        sarif_run = LINTERS[linter](Path(args.target).absolute(), args.output)
+
+        if sarif_run is not None:
+            sarif["runs"].append(sarif_run)
+
+    # output SARIF
+    with open(f"{args.output}", "w") as sf:
+        json.dump(sarif, sf, indent=2)
 
 
 if __name__ == "__main__":
